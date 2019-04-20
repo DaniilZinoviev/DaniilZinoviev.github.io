6e926d18b15a4f2b8edcbefa27d5b0a6b05a5ea0
DaniilZinovyev <44101039+DaniilZinovyev@users.noreply.github.com>
2019-04-20 22:03:58 +0300

    Add one more

---
+++ demos/Questionnaire/js/data.js
@@ -0,0 +1,191 @@
+var data = [
+    {
+        id: 0,
+        question: "Сколько пространства доступно вам?",
+        options: [
+            {
+                userChoice: "Немало",
+                moveTo: 1
+            },
+            {
+                userChoice: "Немного есть",
+                moveTo: 2
+            },
+            {
+                userChoice: "Уголок в квартире",
+                moveTo: 3
+            }
+        ]
+    },
+    {
+        id: 1,
+        question: "Какую пользу больше желаете?",
+        options: [
+            {
+                userChoice: "Передвижение",
+                moveTo: 4
+            },
+            {
+                userChoice: "Яйца",
+                moveTo: 5
+            },
+            {
+                userChoice: "Шерсть",
+                moveTo: 6
+            }
+        ]
+    },{
+        id: 2,
+        question: "Шерсть нужна?",
+        options: [
+            {
+                userChoice: "Да",
+                moveTo: 15
+            },
+            {
+                userChoice: "Нет",
+                moveTo: 7
+            }
+        ]
+    },
+    {
+        id: 3,
+        question: "Должно быть с ушами?",
+        options: [
+            {
+                userChoice: "Да",
+                moveTo: 9
+            },
+            {
+                userChoice: "Никаких ушей",
+                moveTo: 8
+            }
+        ]
+    },
+    {
+        id: 4,
+        result: "лошадь"
+    },
+    {
+        id: 5,
+        question: "Должно хоть немного летать?",
+        options: [
+            {
+                userChoice: "Да, должно",
+                moveTo: 11
+            },
+            {
+                userChoice: "Никаких летать",
+                moveTo: 7
+            }
+        ]
+    },
+    {
+        id: 6,
+        question: "Хотите иметь возможность использовать для рекламы шоколада?",
+        options: [
+            {
+                userChoice: "Нет",
+                moveTo: 13
+            },
+            {
+                userChoice: "Было бы неплохо",
+                moveTo: 12
+            }
+        ]
+    },
+    
+    {
+        id: 7,
+        question: "Ничего, что агрессивный?",
+        options: [
+            {
+                userChoice: "Подойдёт",
+                moveTo: 17
+            },
+            {
+                userChoice: "Не подходит!",
+                moveTo: 16
+            }
+        ]
+    },
+    {
+        id: 8,
+        question: "Что на счёт птички?",
+        options: [
+            {
+                userChoice: "Идёт",
+                moveTo: 5
+            },
+            {
+                userChoice: "Не уверен",
+                moveTo: 10
+            }
+        ]
+    },
+    {
+        id: 9,
+        result: "Кролик"
+    },
+    {
+        id: 10,
+        result: "Пчелы"
+    },
+    {
+        id: 11,
+        result: "Курица"
+    },
+    {
+        id: 12,
+        result: "Корова"
+    },
+    {
+        id: 13,
+        question: "Рогатых животных боитесь?",
+        options: [
+            {
+                userChoice: "Всё ок",
+                moveTo: 14
+            },
+            {
+                userChoice: "Боюсь",
+                moveTo: 15
+            }
+        ]
+    },
+    {
+        id: 14,
+        result: "Коза"
+    },
+    {
+        id: 15,
+        result: "Овца"
+    },
+    {
+        id: 16,
+        result: "Утка"
+    },
+    {
+        id: 17,
+        result: "Индюк"
+    },
+    {
+        id: 18,
+        question: "Подержать?",
+        options: [
+            {
+                userChoice: "Да",
+                moveTo: 19
+            },
+            {
+                userChoice: "Нет",
+                moveTo: 18
+            }
+        ]
+    },
+    {
+        id: 19,
+        result: "Гусь"
+    }
+
+];
